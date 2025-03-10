0029e655743827e46116fd535269821b0a8f3815
mathew <kmk080405@gmail.com>
2025-03-10 13:28:53 +0300

    Week 1 finished

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,7 +3,10 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.*;
 
 public class ArrayListProductDaoTest
 {
@@ -15,7 +18,65 @@ public class ArrayListProductDaoTest
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testFindProductsHasResults() {
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSaveProduct(){
+        Currency usd = Currency.getInstance("USD");
+        Product testproduct = new Product("testproduct", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+
+        try {
+            productDao.save(testproduct);
+        }catch (Exception exception){
+            fail("Unable to save product");
+        }
+
+        Product found = null;
+        try {
+            found = productDao.getProduct(testproduct.getId());
+        }catch (ProductNotFoundException productNotFoundException){
+            fail("Product was not added");
+        }
+
+        assertEquals("testproduct", found.getCode());
+    }
+
+    @Test
+    public void testDeleteProduct(){
+        Currency usd = Currency.getInstance("USD");
+        Product testproduct = new Product("testproduct", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+
+        try {
+            productDao.save(testproduct);
+        }catch (Exception exception){
+            fail("Unable to save product");
+        }
+
+        Product found = null;
+        try {
+            found = productDao.getProduct(testproduct.getId());
+        }catch (ProductNotFoundException productNotFoundException){
+            fail("Product was not added");
+        }
+
+        assertEquals("testproduct", found.getCode());
+
+        try {
+            productDao.delete(found.getId());
+        }catch (ProductNotFoundException productNotFoundException){
+            fail("Unable to find the product for deletion");
+        }
+
+
+        found = null;
+        try {
+            found = productDao.getProduct(testproduct.getId());
+        }catch (ProductNotFoundException productNotFoundException){
+            found = null;
+        }
+
+        assertNull(found);
     }
 }
